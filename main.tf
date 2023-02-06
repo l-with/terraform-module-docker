a1dffdc1cd4dba59b650cdd4423da7fda9474b97
Lars With <lars@with.de>
2023-02-06 13:05:16 +0100

    implement

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "system_file" "etc_docker_daemon_json" {
+  count   = var.docker_manipulate_iptables_rules ? 0 : 1
+  path    = "/etc/docker/daemon.json"
+  mode    = 640
+  content = <<EOT
+{ 
+  "iptables": false 
+}
+    EOT
+}
+
+resource "system_folder" "etc_apt_keyrings" {
+  path = "/etc/apt/keyrings"
+  mode = 755
+}
+
+data "system_command" "install_docker_repository_signing_key" {
+  command = "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg"
+}
+
+data "system_command" "install_docker_reposistory" {
+  depends_on = [
+    data.system_command.install_docker_repository_signing_key
+  ]
+  command = "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | tee /etc/apt/sources.list.d/docker.list > /dev/null"
+}
+
+data "system_command" "apt_cache_update" {
+  depends_on = [data.system_command.install_docker_reposistory]
+  command    = "apt update"
+}
+
+locals {
+  packages = [
+    "docker-ce",
+    "docker-ce-cli",
+    "containerd.io",
+    "docker-compose-plugin",
+  ]
+}
+
+resource "system_packages_apt" "docker_packages" {
+  for_each = toset(local.packages)
+  package {
+    name = each.key
+  }
+}
